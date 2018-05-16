ae883db9e6462d44602da97e54f351969e58bf0b
iguau23 <leonardotkimura@gmail.com>
2018-05-16 17:45:48 -0300

    algumas mensagens interface

---
+++ log_teste.py
@@ -0,0 +1,72 @@
+import logging
+import time
+from threading import Timer
+
+import cflib.crtp  # noqa
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.log import LogConfig
+
+logging.basicConfig(level=logging.ERROR)
+
+
+
+class LoggingExample:
+    def __init__(self, link_uri):
+        self._cf = Crazyflie(rw_cache='./cache')
+
+        # Connect some callbacks from the Crazyflie API
+        self._cf.connected.add_callback(self._connected)
+
+        print('Connecting to %s' % link_uri)
+
+        # Try to connect to the Crazyflie
+        self._cf.open_link(link_uri)
+
+        # Variable used to keep main loop occupied until disconnect
+        self.is_connected = True
+
+    def _connected(self, link_uri):
+        print('Connected to %s' % link_uri)
+
+        self._lg_stab = LogConfig(name='Battery', period_in_ms=100)
+        self._lg_stab.add_variable('pm.vbat', 'float')
+
+
+        try:
+            self._cf.log.add_config(self._lg_stab)
+            # This callback will receive the data
+            self._lg_stab.data_received_cb.add_callback(self._stab_log_data)
+            # Start the logging
+            self._lg_stab.start()
+        except AttributeError:
+            print('Could not add Stabilizer log config, bad configuration.')
+
+        t = Timer(5, self._cf.close_link)
+        t.start()
+
+    def _stab_log_data(self, timestamp, data, logconf):
+        """Callback froma the log API when data arrives"""
+        print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+
+    def _disconnected(self, link_uri):
+        """Callback when the Crazyflie is disconnected (called in all cases)"""
+        print('Disconnected from %s' % link_uri)
+        self.is_connected = False
+
+
+if __name__ == '__main__':
+# Initialize the low-level drivers (don't list the debug drivers)
+    cflib.crtp.init_drivers(enable_debug_driver=False)
+    # Scan for Crazyflies and use the first one found
+    print('Scanning interfaces for Crazyflies...')
+    available = cflib.crtp.scan_interfaces()
+    print('Crazyflies found:')
+    for i in available:
+        print(i[0])
+
+    if len(available) > 0:
+        le = LoggingExample(available[0][0])
+    else:
+        print('No Crazyflies found, cannot run example')
+    while le.is_connected:
+        time.sleep(1)
--- mini_drone_paralelo.py
+++ mini_drone_paralelo.py
@@ -19,13 +19,12 @@ factory = CachedCfFactory(rw_cache='./cache')
 
 
 
-position   = [1]
+dronePosition   = [1, 3, 5]
 cornersList = [1,3,5,7]
 validCommmand = True
 user_input_numbers = 'start'
 user_input_command = 'start'
-messageComando =("Escolha um comando:\n"
-                 "(1) - arco\n"
+messageComando =("(1) - arco\n"
                  "(2) - degrau\n"
                  "(3) - linear\n"
                  "(4) - ziguezague\n"
@@ -42,12 +41,18 @@ while(user_input_command != 's'):
     user_input_numbers = input(message)
 
     selected = []
-    if(user_input.find('1')!=-1):
+    if(user_input_numbers.find('1')!=-1):
         selected.append(1)
-    if(user_input.find('2')!=-1):
+    if(user_input_numbers.find('2')!=-1):
         selected.append(2)
-    if(user_input.find('3')!=-1):
+    if(user_input_numbers.find('3')!=-1):
         selected.append(3)
+    if(len(selected)==0):
+        print("\n===============================================================================\n"
+              "     Obrigado por terem utilizado o nosso mini-drones program!\n"
+              "                      Espero que tenham gostado!\n"
+              "===============================================================================\n")
+        break
 
     scf = []
     mcs = []
@@ -59,31 +64,32 @@ while(user_input_command != 's'):
         scf.append(sync)
 
     selectedCommands = []
-    for j in selected:
-        print("\npara o drone %d" %j)
+    for i in selected:
+        print("\nEscolha um comando para o drone %d, que está na posição %d" %(i, dronePosition[i-1]))
         selectedCommands.append(input(messageComando))
-        position[j-1] = pc.newPosition(position[j-1])
+        dronePosition[i-1] = pc.newPosition(dronePosition[i-1])
 
-
+    print("executando")
     pr = paralelo.Paralelo(mcs)
 
     for mc in mcs:
         pr.putCommand(paralelo.TAKEOFF, mc)
-    pr.execute()
+    #pr.execute()
 
-    for k in selected:
-        pr.putCommand(selectedCommands[k-1], mcs[k-1])
-    pr.execute()
+    for i in range(len(selected)):
+        pr.putCommand(selectedCommands[i-1], mcs[i-1])
+    #pr.execute()
 
-    for m in selected:
-        if(position[m-1] in cornersList):
-            pr.putCommand(paralelo.TURNRIGHT, mcs[l-1])
-        pr.execute()
+    for i in range(len(selected)):
+        if(dronePosition[i-1] in cornersList):
+            pr.putCommand(paralelo.TURNRIGHT, mcs[i-1])
+        #pr.execute()
 
 
-    for l in selected:
-        pr.putCommand(paralelo.LAND, mcs[l-1])
-    pr.execute()
+    for i in range(len(selected)):
+        pr.putCommand(paralelo.LAND, mcs[i-1])
+    #pr.execute()
+    print("pronto\n")
 
     for sync in scf:
         sync.close_link()
