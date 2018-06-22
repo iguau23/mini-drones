a685f48954f8f6728a1c696f64b4cdc1c6bec627
Toshi <leonardotkimura@gmail.com>
2018-06-21 22:59:02 -0300

    deletar alguns arquivos não essenciais

---
+++ ciab/servidor.py
@@ -0,0 +1,114 @@
+import requests
+import time
+import threading
+from pynput.keyboard import Key, Controller
+
+
+URL_POP = 'https://3vkeycenej.execute-api.us-east-1.amazonaws.com/prod/CIAB-2018-DroneCommandQueue?droneId=drone1&operation=POP&secret=dr0neRulez2A5T7U'
+URL_PEEK = 'https://3vkeycenej.execute-api.us-east-1.amazonaws.com/prod/CIAB-2018-DroneCommandQueue?droneId=drone1&operation=PEEK&secret=dr0neRulez2A5T7U'
+
+URL_PUSH_STATUS = 'https://3vkeycenej.execute-api.us-east-1.amazonaws.com/prod/CIAB-2018-DroneCommandQueue?operation=PUSH&secret=dr0neRulez2A5T7U&droneId=status&command='
+
+class Servidor():
+    def __init__(self):
+        self.cancelar_prepara = False
+        self.comando = "empty"
+        self.cancelarConexao = False
+        self.executando = False
+        self.semConexao = True
+
+    def setExecutando(self, value):
+        self.executando = value;
+        #r = request.get()
+
+    def solicitar_conexao(self):
+        print("aguardando comando...")
+        #joga fora o primeiro comando que estiver na pilha
+        r = requests.get(URL_POP)
+
+        r = requests.get(URL_POP)
+        while(r.text=='empty'):
+            if(self.cancelarConexao==True):
+                return
+
+            #Verifica se não houve perda de conexão
+            for cf in self.mTeste.cfs:
+                if (cf.link == None):
+                    print("conexao com o drone perdida")
+                    self.cancelarConexao = True
+                    self.semConexao = True
+
+            r = requests.get(URL_POP)
+            time.sleep(1)
+
+        self.comando = r.text
+        Servidor.press_enter()
+
+    def cancela_comando(self):
+        entrada = input("tecle ENTER se quiser cancelar: ")
+        while(not self.cancelarConexao):
+            if(entrada==""):
+                self.cancelarConexao = True
+                return
+
+
+    def verificar_comando(self, mTeste):
+        self.mTeste = mTeste
+        threadComando = threading.Thread(target=self.solicitar_conexao)
+        threadComando.setDaemon(True)
+
+        threadCancela = threading.Thread(target=self.cancela_comando)
+        threadCancela.setDaemon(True)
+
+        threadComando.start()
+        threadCancela.start()
+
+        threadComando.join()
+
+
+
+    def verificar_prepara(self):
+        threadEsperaPrepara = threading.Thread(target=self.espera_prepara)
+        threadEsperaPrepara.setDaemon(True)
+
+        threadContinua = threading.Thread(target=self.cancela_prepara)
+        threadContinua.setDaemon(True)
+
+        threadEsperaPrepara.start()
+        threadContinua.start()
+
+        threadEsperaPrepara.join()
+
+
+
+    def espera_prepara(self):
+        print("\n===============================================================================\n"
+              "Aguardando prepara...")
+        #joga fora o primeiro comando da pilha
+        r = requests.get(URL_POP)
+
+        r = requests.get(URL_POP)
+        while(r.text!= "preparar"):
+            if(self.cancelar_prepara):
+                return
+            r = requests.get(URL_POP)
+            time.sleep(1)
+        Servidor.press_enter()
+
+
+    def cancela_prepara(self):
+        while(not self.cancelar_prepara):
+            entrada = input("tecle ENTER se quiser continuar: "
+                            "\n===============================================================================\n")
+            if(entrada==""):
+                self.cancelar_prepara = True
+                break
+
+
+    def push_status(command):
+        r = requests.get(URL_PUSH_STATUS + command)
+
+    def press_enter():
+        keyboard = Controller()
+        keyboard.press(Key.enter)
+        keyboard.release(Key.enter)
---
+++ ciab/trajetorias.py
@@ -0,0 +1,163 @@
+import logging
+import time
+import math
+from cflib.positioning.motion_commander import MotionCommander
+
+DIST = 0.8
+def turnRight(mc):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    mc.turn_left(90)
+    time.sleep(1)
+
+def turnLeft(mc):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    mc.turn_left(90)
+    time.sleep(1)
+
+def linear(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.forward(1.5*dist, velocity=0.5)
+    time.sleep(1)
+
+def arco(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.turn_left(90)
+    mc.circle_right(0.75*dist, velocity=0.6, angle_degrees=180)
+    mc.forward(0.25*dist, velocity=0.6)
+    mc.turn_left(90)
+    time.sleep(1)
+
+def circulo(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.forward(1.2*dist, velocity=0.7)
+    mc.circle_left(1.3*dist, velocity=0.7, angle_degrees=180)
+    mc.circle_left(1.0*dist, velocity=0.7, angle_degrees=180)
+    mc.circle_left(0.7*dist, velocity=0.7, angle_degrees=180)
+    time.sleep(1)
+    mc.turn_left(180)
+    time.sleep(1)
+    mc.circle_right(0.7*dist, velocity=0.7, angle_degrees=180)
+    mc.circle_right(1.0*dist, velocity=0.7, angle_degrees=180)
+    mc.circle_right(1.3*dist, velocity=0.7, angle_degrees=180)
+    time.sleep(1)
+
+def zigueZague(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.move_distance(0.4*dist, 0.25, 0, velocity=0.4)
+    time.sleep(1)
+    mc.move_distance(0.4*dist, -0.25, 0, velocity=0.4)
+    time.sleep(1)
+    mc.move_distance(0.4*dist, 0.25, 0, velocity=0.4)
+    time.sleep(1)
+    mc.move_distance(0.4*dist, -0.25, 0, velocity=0.4)
+    time.sleep(1)
+
+def degrau(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.forward(0.4*dist, velocity=0.5)
+    mc.up(0.2)
+    time.sleep(1)
+    mc.forward(0.3*dist, velocity=0.5)
+    mc.down(0.2)
+    time.sleep(1)
+    mc.forward(0.3*dist, velocity=0.5)
+    mc.up(0.2)
+    mc.forward(0.4*dist, velocity=0.5)
+    time.sleep(1)
+
+def loop(mc, dist = DIST):
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    time.sleep(1)
+    mc.forward(0.8*dist, velocity=0.5)
+    time.sleep(1)
+    #loop
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    velocity = 0.2
+    radius_m = 0.2
+    angular_velocity = velocity/radius_m
+    theta = 0
+    t = 0
+    reflesh_rate = 0.05
+    start_angle = -math.pi/2
+    while (theta < (2*math.pi)):
+        velocity_x = -angular_velocity*radius_m*math.sin(theta + start_angle)
+        velocity_z = angular_velocity*radius_m*math.cos(theta + start_angle)
+        mc.start_linear_motion(velocity_x, 0.0, velocity_z)
+        t += reflesh_rate
+        time.sleep(reflesh_rate)
+        theta = angular_velocity*t
+    mc.stop()
+    #fim do loop
+    time.sleep(1)
+    mc.forward(0.7*dist, velocity=0.5)
+
+def espiral(mc, dist = DIST):
+    #parametros
+    velocity = 0.2
+    velocity_circle = 0.3
+    radius_m = 0.15
+    distance = 1.4*dist
+    angular_velocity = velocity_circle/radius_m
+    flight_time = distance/velocity
+    theta = 0
+    t = 0
+    reflesh_rate = 0.05
+    start_angle = -math.pi/2
+
+    #inicio da espiral
+    if(mc.getStopMotion()):
+        if(mc._is_flying):
+            mc.land()
+        return
+    while (t < flight_time):
+        velocity_y = -angular_velocity*radius_m*math.sin(theta + start_angle)
+        velocity_z = angular_velocity*radius_m*math.cos(theta + start_angle)
+        mc.start_linear_motion(velocity, velocity_y, velocity_z)
+        t += reflesh_rate
+        mc.safeSleep(reflesh_rate)
+        theta = angular_velocity*t
+    voltas_completas = (math.pi*2)*(theta//(math.pi*2))
+    theta -= voltas_completas
+    mc.stop()
+
+    #termina a ultima rotacao
+    while (theta<math.pi*2):
+        velocity_y = -angular_velocity*radius_m*math.sin(theta + start_angle)
+        velocity_z = angular_velocity*radius_m*math.cos(theta + start_angle)
+        mc.start_linear_motion(0.0, velocity_y, velocity_z)
+        t += reflesh_rate
+        time.sleep(reflesh_rate)
+        theta = angular_velocity*t - voltas_completas
+    mc.stop()
